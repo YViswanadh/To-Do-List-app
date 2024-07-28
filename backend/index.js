89c9c16805af3512547a27d263c4883a5549d066
YViswanadh <viswanadh0828@gmail.com>
2024-07-28 19:24:36 +0530

    todo app backend done

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,48 @@
 const express = require('express');
 const { createTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 
 app.use(express.json());
 
-app.post('/todo',(req,res)=>{
+app.post('/todo',async (req,res)=>{
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
         res.status(400).send(parsedPayload.error.issues[0].message);
+        return ;
     }
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed: false,
+    })
+    res.json({
+        msg:"Todo created"
+    })
     res.status(201).send(parsedPayload.success);
 })
 
-app.get('/todos',(req,res)=>{
+app.get('/todos',async (req,res)=>{
+    const todos = await todo.find({});
 
+    res.json({
+        todos
+    })
 })
 
 
-app.put('/completed',(req,res)=>{
+app.put('/completed',async (req,res)=>{
     const updatedPayload = req.body;
     const parsedPayload = createTodo.safeParse(updatedPayload);
     if(!parsedPayload.success){
         res.status(400).send(parsedPayload.error.issues[0].message);
     }
+    await todo.update({
+        _id : req.body.id,
+    },{
+        completed : true
+    })
     res.status(201).send(parsedPayload.success);
 })
 
