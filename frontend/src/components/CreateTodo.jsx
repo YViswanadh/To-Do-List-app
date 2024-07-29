d2b84f0ebfaee6baa9c29c9a17af289a01dfaaeb
YViswanadh <viswanadh0828@gmail.com>
2024-07-30 04:34:58 +0530

    frontend

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,8 +1,19 @@
-function CreateTodo(){
-    return <div>
-        <input type="text" placeholder="title"></input><br/>
-        <input type="text" placeholder="description"></input><br/>
 
-        <button>Add a todo</button>
+
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder="title" style={{
+            padding:10,
+            margin : 10
+        }}></input><br/>
+        <input type="text" placeholder="description" style={{
+            padding:10,
+            margin : 10
+        }}></input><br/>
+
+        <button style={{
+            padding:10,
+            margin : 10
+        }}>Add a todo</button>
     </div>
 }
